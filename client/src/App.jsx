7e0ea46a3fdbe4659f0035e0a6accbe1b982fccc
yogeshk07 <hkuruvanshi227@gmail.com>
2024-05-24 12:15:13 +0530

    create pages and route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,22 @@
 import { useState } from "react";
-
-
+import Home from "./pages/Home";
+import SignUp from "./pages/SignUP";
+import SignIn from "./pages/SignIn";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
       <h1>qeiijip</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
     </>
   );
 }
